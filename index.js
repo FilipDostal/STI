658c1b31864b531d346ab9bf3924bb8605fb9f4d
Filip Dostál <filip.dostal@tul.cz>
2022-05-25 20:14:12 +0200

    Added recommendation feature
    Added feature to recommend buying euros depending on 3 day changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 //const bckend = require('./backend');
 const apiKey = "STINPKS20XXII";
-
+/* istanbul ignore next */
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
   .get('/awake', (req, res) => res.send("awake"))
   .get('/api', (req, res) => {
     //commandResolver(req.query.command).then(clb => {res.send(clb)});
-    res.send(commandResolver(req.query.command,req.query.lang));
+    res.send(commandResolver(req.query.command, req.query.lang));
   })
   .listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 
-function commandResolver(cmdString,lang) {
+function commandResolver(cmdString, lang) {
   if (cmdString === '!rate' || cmdString.includes('rate') || cmdString.includes('kurz') || cmdString.includes('euro') || cmdString.includes('EUR')) {
 
     return showRate(lang);
@@ -56,7 +56,11 @@ function commandResolver(cmdString,lang) {
     return showHistory(lang);
 
   }
-  if(lang == "0") return "I dont know what you mean";
+  if (cmdString === '!rec' || cmdString.includes('doporučit') || cmdString.includes('nákup') || cmdString.includes('buy') || cmdString.includes('recommend')) {
+    return showRecommend(lang);
+
+  }
+  if (lang == "0") return "I dont know what you mean";
   return "Tak tohle neznám";
 }
 
@@ -64,37 +68,77 @@ function showHistory(lang) {
   return rate_data;
 }
 
+let lastDays = [20.0, 20.0, 20.0];
+function shiftArray(number) {
+  lastDays[0] = lastDays[1];
+  lastDays[1] = lastDays[2];
+  lastDays[2] = number;
+}
+
+let recomend = 0; // 0 - doporucit| 1 - nedoporučit
+let avrg = 20.0;
+function calcRecomendation() {
+  avrg = (parseFloat(lastDays[0]) + parseFloat(lastDays[1]) + parseFloat(lastDays[2])) / 3;
+  if ((lastDays[2] - lastDays[1]) > (avrg * 0.1) || (lastDays[0] - lastDays[1]) > (avrg * 0.1)) recomend = 1;
+
+}
+
+function showRecommend(lang){
+  return loadRecommend(lang,recomend);
+
+}
+
+function loadRecommend(lang,rec){
+  if(lang == "0"){
+      if(rec == 0) {return "Recommended! " + buildString()};
+        return "Not Recommended! " + buildString();
+  }
+  if(rec == 0) return "Doporučuji! " + buildString()
+  return "Nedoporučuji! " + buildString()
+}
+
+function buildString(){
+  console.log("AVG " + avrg);
+return "AVG: " + avrg +"("+ (avrg*0.1)+")"+ " 3>2: " + (lastDays[2] - lastDays[1]) + " 2>1: " + (lastDays[0] - lastDays[1]);
+}
+
 let rate_data;
 async function loadHistory(lang) {
+  /* istanbul ignore next */
   try {
     const client = await pool.connect();
     const result = await client.query("SELECT date_trunc('day',posting_date)::date as date, rate from rate_history;");
     let results = "";
     result.rows.forEach(elelment => {
       let toCrop = elelment.date + " ";
-      results = results + toCrop.substring(0,15) + " : " + elelment.rate + "\n"
+      results = results + toCrop.substring(4, 10) + " : " + elelment.rate + "|\n";
+      shiftArray(elelment.rate);
     })
     //const results = { 'results': (result) ? result.rows : null };
     rate_data = results;
-    client.release();
+    //client.release();
+    calcRecomendation();
   }
   catch (err) {
     console.log("Cannot connect to DB");
+    client.release();
   }
 }
 
 function showTime(lang) {
   let date_ob = new Date();
-  let hours = date_ob.getHours() +2 ;
+  let hours = date_ob.getHours() + 2;
   let minutes = date_ob.getMinutes();
   let seconds = date_ob.getSeconds();
-  if(lang == "0") return  "Current time is " + hours + ":" + minutes + ":" + seconds + ".";
+  if (lang == "0") return "Current time is " + hours + ":" + minutes + ":" + seconds + ".";
   return "Momentální čas je " + hours + ":" + minutes + ":" + seconds + ".";
 }
 showName = (lang) => {
-  if(lang == "0") return "My name is ChatBot()";
+  if (lang == "0") return "My name is ChatBot()";
   return "Moje jméno je ChatBot()";
 }
+
+
 
 
 function showRate(lang) {
@@ -104,7 +148,7 @@ function showRate(lang) {
 }
 
 const jobRefreshRate = scheduler.scheduleJob('30 * * * *', function () { callOER() });
-
+/* istanbul ignore next */
 const jobDailyRate = scheduler.scheduleJob('00 06 * * *', async function () {
   try {
     let conversion = fx.convert(1, { from: "EUR", to: "CZK" });
@@ -117,8 +161,11 @@ const jobDailyRate = scheduler.scheduleJob('00 06 * * *', async function () {
   }
 });
 
+
+
 function callOER() {
   let url = "https://openexchangerates.org/api/latest.json?app_id=7a3348c295b6434ba94febe7f57c76f0";
+  /* istanbul ignore next */
   try {
     https.get(url, (resp) => {
       let body = "";
@@ -143,6 +190,7 @@ function callOER() {
   catch (err) { }
 
 }
+
 const jobKeepAwake = scheduler.scheduleJob('*/30 * * * *', function () { https.get("https://stin-backend.herokuapp.com/awake", (resp) => { console.log("keeping awake"); }) });
 
-module.exports = { showTime, showName, showRate, commandResolver, callOER, loadHistory, showHistory, fx }
+module.exports = { showTime, showName, showRate, commandResolver, loadRecommend, callOER, loadHistory, showHistory, recomend, fx, showRecommend, buildString, calcRecomendation, calcRecomendation, lastDays, shiftArray }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { spawn, ChildProcess } = require('child_process');
+const { spawn } = require('child_process');
 const got = require('got');
 const fx = require('money');
 const test = require('tape');
@@ -15,9 +15,37 @@ test('show name', (t) => {
   t.end();
 });
 
+test('string builder', (t) => {
+  t.plan(1);
+  t.equal(bckend.buildString(),"AVG: 20(2) 3>2: 0 2>1: 0");
+  t.end();
+});
+
+test('call  recomend', (t) => {
+  t.plan(2);
+  t.equal(bckend.showRecommend("0"),"Recommended! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.equal(bckend.showRecommend("1"),"Doporučuji! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.end();
+});
+
+test('shifty array', (t) => {
+  t.plan(1);
+  bckend.shiftArray(20);
+  t.equal(bckend.buildString(),"AVG: 20(2) 3>2: 0 2>1: 0");
+  t.end();
+});
+
+test('show recommend', (t) => {
+  t.plan(4);
+  t.equal(bckend.loadRecommend("0",0),"Recommended! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.equal(bckend.loadRecommend("1",0),"Doporučuji! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.equal(bckend.loadRecommend("0",1),"Not Recommended! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.equal(bckend.loadRecommend("1",1),"Nedoporučuji! AVG: 20(2) 3>2: 0 2>1: 0");
+  t.end();
+});
+
 test('load history', (t) => {
   t.plan(1);
-  
   t.notEqual(bckend.loadHistory(),undefined);
   t.end();
 
@@ -51,7 +79,7 @@ test('show time', (t) => {
 
 
 test('test command resolver', (t) => {
-  t.plan(8);
+  t.plan(9);
   t.equal(bckend.commandResolver("!hello","1"),"Moje jméno je ChatBot()");
   t.equal(bckend.commandResolver("!hello","0"),"My name is ChatBot()");
 
@@ -64,6 +92,7 @@ test('test command resolver', (t) => {
   t.throws(function() {bckend.commandResolver("!rate")});
   t.equal(bckend.commandResolver("!history"),undefined);
   t.equal(bckend.commandResolver("asdad","0"),"I dont know what you mean");
+  t.equal(bckend.commandResolver("!rec","0"),"Recommended! AVG: 20(2) 3>2: 0 2>1: 0");
   t.equal(bckend.commandResolver("asdad","1"),"Tak tohle neznám");
   t.end();
   child.kill();
@@ -74,29 +103,26 @@ test('test command resolver', (t) => {
   t.equal(true,true);
   setTimeout(function() {}, 5000);
   process.exit(1);});*/
+/*
+  test('responds to requests', (t) => {
+    t.plan(4);
+  
+    // Wait until the server is ready
+    child.stdout.on('data', _ => {
+      // Make a request to our app
+      request('http://127.0.0.1:5000', (error, response, body) => {
+        // stop the server
+        child.kill();
+  
+        // No error
+        t.false(error);
+        // Successful response
+        t.equal(response.statusCode, 200);
+        // Assert content checks
+        t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
+        t.notEqual(body.indexOf("Getting Started on Heroku with Node.js"), -1);
+      });
+    });
+  });*/
 
 test.onFinish(() => process.exit(0));
-/*
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
-
-test('responds to requests', (t) => {
-  t.plan(4);
-
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
-
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    });
-  });
-});*/
